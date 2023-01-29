59ea7ebb72711c665185860e9eaa86bbd748d5ad
indraneeldey2000 <116252120+indraneeldey2000@users.noreply.github.com>
2023-01-29 22:54:49 +0530

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {useNavigate} from "react-router-dom"
+//import { Link } from "react-router-dom";
 const Blog = () => {
-
+ const navigate = useNavigate()
 
     return (
         <div id="blog-page">
